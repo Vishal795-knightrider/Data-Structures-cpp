e13b2c8ad4786aa0b4c14d6adab3b01b3bcd010d
Vishal795-knightrider <vk3293801@gmail.com>
2026-08-18 23:54:05 +0530

    added deci to bin in bit maipulation

--- bit.cpp
+++ bit.cpp
@@ -1,20 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// convert bin to decimal
+// convert deci to binary
 
-string bin2deci(int n){
-    string res="";
-    while(n!=0){
-        if(n%2==1) res+='1';
-        else res+='0';
-        n=n/2;
+// string deci2bin(int n){
+//     string res="";
+//     while(n!=0){
+//         if(n%2==1) res+='1';
+//         else res+='0';
+//         n=n/2;
+//     }
+//     reverse(res.begin(),res.end());
+//     return res;
+// }
+
+// int main(){
+//     int n; cin >> n;
+//     cout << deci2bin(n);
+// }
+
+
+//convert bin to decimal
+
+int dec2bin(string str){
+    int num=0;
+    int n=str.size()-1;
+    int powerof2=1;
+    for(int i=n;i>=0;i--){
+        if(str[i]=='1'){
+            num=num+powerof2;
+        }
+        powerof2 *=2;
     }
-    reverse(res.begin(),res.end());
-    return res;
+    return num;
 }
-
 int main(){
-    int n; cin >> n;
-    cout << bin2deci(n);
+    string str; cin >> str;
+    cout << dec2bin(str);
 }
