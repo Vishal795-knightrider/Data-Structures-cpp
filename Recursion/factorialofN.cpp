f696b0618badd65a6f2cd4d90eb1498b3f9385cc
Vishal795-knightrider <vk3293801@gmail.com>
2026-08-29 15:10:06 +0530

    fact n and reverse

--- Recursion/factorialofN.cpp
+++ Recursion/factorialofN.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int fact(int n){
-    if(n==1 || n==0) return 1;                //o factorial bhi 1 hota hai0
+    if(n==1 || n==0) return 1;   //we know that factorial of 1 is 1 so fact(1) shoudl return 1       //o factorial bhi 1 hota hai0
     return n*fact(n-1);
 }
 
 int main(){
     int n; cin >> n;
-    cout << fact(n);
+    cout << fact(n);                //what is fact(n) means ?? it means factorial of n numbers
 }
---
+++ Recursion/reverse_array.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// void rev(int l,int r,int arr[]){
+//     if(l>=r) return;
+//     swap(arr[l],arr[r]);
+//     rev(l+1,r-1,arr);
+// }
+
+// int main(){
+//     int arr[]={1,2,3,4,5,6,7};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+//     rev(0,n-1,arr);
+//     for(auto it:arr){
+//         cout << it << " ";
+//     }
+// }
+
+
+// void rev(int i,int n,int arr[]){
+//     if(i>=n/2) return;
+//     swap(arr[i],arr[n-i-1]);
+//     rev(i+1,n,arr);
+// }
+
+// int main(){
+//     int arr[]={1,2,3,4,5,6,7};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+//     rev(0,n,arr);
+//     for(auto it:arr){
+//         cout << it << " ";
+//     }
+// }
