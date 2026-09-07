8dd7d57065ddc46a94c5ab0a3075e6a47eeaffa7
Vishal Kashyap <vk3293801@gmail.com>
2026-09-07 21:19:00 +0530

    Enable user input for array in subsetSum1.cpp
    fixed program to accept user input for the array

--- Recursion/subsetSum1.cpp
+++ Recursion/subsetSum1.cpp
@@ -11,8 +11,11 @@ void fn(int idx,int sum,int arr[],vector<int> &subsetSum,int n){
 }
 
 int main(){
-  int arr[]={1,2,3};
-  int n=sizeof(arr)/sizeof(arr[1]);
+  int n; cin >> n;
+  int arr[n];
+  for(int i=0;i<n;i++){
+    cin >> arr[i];
+  }
   vector<int> subsetSum;
   fn(0,0,arr,subsetSum,n);
   sort(subsetSum.begin(),subsetSum.end());
