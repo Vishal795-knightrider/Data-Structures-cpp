12f60540bb2e4b7004c4a5a25f651ee73c013da3
Vishal795-knightrider <vk3293801@gmail.com>
2026-09-11 23:45:43 +0530

    resolve bug

--- Two pointer n Sliding recursion/longest substring without repeating characters/long_substring.cpp
+++ Two pointer n Sliding recursion/longest substring without repeating characters/long_substring.cpp
@@ -3,15 +3,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+//Brute force
 int main(){
   string str="cabx^ssydk,s";
   int n=str.size();
 
-  int hash[255]={0};
+  
 
   int maxlen=0;
   for(int i=0;i<n;i++){
+    int hash[255]={0};
     for(int j=i;j<n;j++){          
       if(hash[s[j]]==1) break;
       maxlen=max(maxlen,j-i+1);
